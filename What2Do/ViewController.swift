0870a93d3c75a04066da6f1c604cbb078d81c5fa
Vinojen Gengatharan <vinnymac@Vinojens-Mac-mini.local>
2020-10-25 12:01:31 -0400

    First Commit

---
+++ What2Do/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  What2Do
+//
+//  Created by Vinojen Gengatharan on 2020-10-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
