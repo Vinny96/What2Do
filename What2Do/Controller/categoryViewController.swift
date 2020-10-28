b7bc4de9d56d23302b5838f80371851047ff7fdb
Vinojen Gengatharan <vinnymac@Vinojens-Mac-mini.local>
2020-10-28 09:43:37 -0400

    Implemented colour functionality to category cells. Color persists as well

---
+++ What2Do/Controller/categoryViewController.swift
@@ -0,0 +1,222 @@
+//
+//  ViewController.swift
+//  What2Do
+//
+//  Created by Vinojen Gengatharan on 2020-10-25.
+//
+
+import UIKit
+import CoreData
+
+class categoryViewController: UITableViewController {
+
+    // variables
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var categories : [Category] = []
+    var cellIndexPath : IndexPath?
+    var initialCellIndexPath : IndexPath?
+    let colorPicker  = UIColorPickerViewController()
+    var cellColor : UIColor?
+    var cellColorAsHex : String?
+    
+    override func viewDidLoad() {
+        //print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        tableView.rowHeight = 50.0
+        
+        super.viewDidLoad()
+        colorPicker.delegate = self
+        loadCategories()
+    }
+    
+    // IB Actions
+    @IBAction func addCategory(_ sender: UIBarButtonItem)
+    {
+        var textField = UITextField()
+        let firstAlertController = UIAlertController(title: "Add new category", message: "", preferredStyle: .alert)
+        let secondAlertController = UIAlertController(title: "Choose colour for cell.", message: "", preferredStyle: .alert)
+        let secondAlertAction = UIAlertAction(title: "Choose your colour", style: .default) { (secondAlertAction) in
+            self.selectColor()
+        }
+        secondAlertController.addAction(secondAlertAction)
+        
+        firstAlertController.addTextField { (alertTextField) in
+            textField = alertTextField
+            textField.placeholder = "Enter new name here."
+        }
+        let firstAlertAction = UIAlertAction(title: "Enter new category", style: .default) { (firstAlertAction) in
+            if(textField.text != "")
+            {
+                let categoryToAdd = Category(context: self.context)
+                self.categories.append(categoryToAdd)
+                categoryToAdd.title = textField.text
+                self.saveCategories()
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                    self.present(secondAlertController, animated: true, completion: nil)
+                }
+            }
+        }
+        firstAlertController.addAction(firstAlertAction)
+        present(firstAlertController, animated: true, completion: nil)
+        
+    }
+    
+    
+    // MARK: - Tableview Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        cellIndexPath = indexPath
+        performSegue(withIdentifier: "categoryToItems", sender: self)
+    }
+    
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete
+        {
+            context.delete(categories[indexPath.row])
+            categories.remove(at: indexPath.row)
+            saveCategories()
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    // MARK: - Tableview Datasource Methods
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath)
+        cell.textLabel?.text = categories[indexPath.row].title
+        initialCellIndexPath = indexPath
+        if let safeRgb = categories[indexPath.row].hexVal
+        {
+            cell.backgroundColor = UIColor(hex: safeRgb)
+        }
+        cell.textLabel?.numberOfLines = 0
+        return cell
+    }
+    
+    // MARK: - Functions
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationSegue : itemsViewController = segue.destination as! itemsViewController
+        if let safeCellIndexPath = cellIndexPath
+        {
+            destinationSegue.fromCategory = categories[safeCellIndexPath.row]
+        }
+    }
+    
+    func selectColor()
+    {
+        colorPicker.supportsAlpha = true
+        present(colorPicker, animated: true, completion: nil)
+    }
+    
+    
+    // MARK: - CRUD Implementation
+    func saveCategories()
+    {
+        do
+        {
+            try context.save()
+        }catch
+        {
+            print(error.localizedDescription)
+            print("Error in saving the categories.")
+        }
+    }
+    
+    func loadCategories(fetchRequest : NSFetchRequest<Category> = Category.fetchRequest() )
+    {
+        do
+        {
+            categories = try context.fetch(fetchRequest)
+            tableView.reloadData()
+        }catch
+        {
+            print(error.localizedDescription)
+            print("There was an error in loading the categories.")
+        }
+    }
+}
+
+//MARK: - UIColorPickerViewControllerDelegate
+extension categoryViewController : UIColorPickerViewControllerDelegate
+{
+    func colorPickerViewControllerDidFinish(_ viewController: UIColorPickerViewController) {
+        print("The colorPicker is being dismissed.")
+        tableView.reloadData()
+    }
+    
+    func colorPickerViewControllerDidSelectColor(_ viewController: UIColorPickerViewController) {
+        cellColor = viewController.selectedColor
+        cellColorAsHex = cellColor?.toHex()
+        if let safeIndexPath = initialCellIndexPath
+        {
+            categories[safeIndexPath.row].hexVal = cellColorAsHex
+            saveCategories()
+        }
+    }
+}
+
+
+//MARK: - UIColor Extension
+@available(iOS 14.0, *)
+extension UIColor
+{
+    convenience init?(hex:String)
+    {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+        var rgb: UInt64 = 0
+        var r : CGFloat = 0.0
+        var g : CGFloat = 0.0
+        var b : CGFloat = 0.0
+        var a : CGFloat = 1.0
+        let length  = hexSanitized.count
+        guard Scanner(string: hexSanitized).scanHexInt64(&rgb) else
+        {return nil}
+        
+        if length == 6 {
+            r = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+            g = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+            b = CGFloat(rgb & 0x0000FF) / 255.0
+
+        } else if length == 8 {
+            r = CGFloat((rgb & 0xFF000000) >> 24) / 255.0
+            g = CGFloat((rgb & 0x00FF0000) >> 16) / 255.0
+            b = CGFloat((rgb & 0x0000FF00) >> 8) / 255.0
+            a = CGFloat(rgb & 0x000000FF) / 255.0
+
+        } else {
+            return nil
+        }
+        self.init(red : r, green : g, blue : b, alpha : a)
+    }
+    
+    // converting a UIColor to Hex in Swift
+    func toHex(alpha : Bool = false) -> String?
+    {
+        guard let components = cgColor.components, components.count >= 3 else
+        {return nil}
+        let r = Float(components[0])
+        let g = Float(components[1])
+        let b = Float(components[2])
+        var a = Float(1.0)
+        if components.count >= 4 {
+            a = Float(components[3])
+        }
+        if alpha {
+            return String(format: "%02lX%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255), lroundf(a * 255))
+        } else {
+            return String(format: "%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255))
+        }
+    }
+}
+
--- What2Do/Controller/itemsViewController.swift
+++ What2Do/Controller/itemsViewController.swift
@@ -6,84 +6,134 @@
 //
 
 import UIKit
+import CoreData
 
 class itemsViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    // variables
+    var items : [Item] = []
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var fromCategory : Category?
+    {
+       didSet
+       {
+           loadItems()
+       }
+       
     }
-
+   
+     
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+    }
+    
+    // MARK: - IB Actions
+    @IBAction func addItem(_ sender: UIBarButtonItem)
+    {
+        var textField = UITextField()
+        let firstAlertController = UIAlertController(title: "Add Item", message: "", preferredStyle: .alert)
+        firstAlertController.addTextField { (alertTextField) in
+            textField = alertTextField
+            textField.placeholder = "Enter item name here."
+        }
+        let firstAlertAction = UIAlertAction(title: "Add new item", style: .default) { (firstAlertAction) in
+            if(textField.text != "")
+            {
+                let itemToAdd = Item(context: self.context)
+                itemToAdd.name = textField.text
+                itemToAdd.parentCategory = self.fromCategory
+                itemToAdd.isDone = false
+                self.saveItems()
+                self.items.append(itemToAdd)
+                self.tableView.reloadData()
+            }
+        }
+        firstAlertController.addAction(firstAlertAction)
+        present(firstAlertController, animated: true, completion: nil)
+    }
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return items.count
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+    // MARK: - Tableview Delegate Methods
+     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row].name
+        if(items[indexPath.row].isDone == true)
+        {
+            cell.accessoryType = .checkmark
+        }
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cellToConfigure = tableView.cellForRow(at: indexPath)
+        if(cellToConfigure?.accessoryType == UITableViewCell.AccessoryType.none)
+        {
+            cellToConfigure?.accessoryType = UITableViewCell.AccessoryType.checkmark
+            items[indexPath.row].isDone = true
+            saveItems()
+        }
+        else
+        {
+            cellToConfigure?.accessoryType = UITableViewCell.AccessoryType.none
+            items[indexPath.row].isDone = false
+            saveItems()
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
+    
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+       
+    }
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
+        if editingStyle == .delete
+        {
+            context.delete(items[indexPath.row])
+            saveItems()
+            items.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    // MARK: - CRUD Functionality
+    func saveItems()
+    {
+        do
+        {
+            try context.save()
+        }catch
+        {
+            print(error.localizedDescription)
+            print("Error in saving the items")
+        }
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+    
+    func loadItems(fetchRequest : NSFetchRequest<Item> = Item.fetchRequest())
+    {
+        if let safeCategory = fromCategory
+        {
+            let sortDescriptor = NSSortDescriptor(key: "parentCategory.title", ascending: true)
+            fetchRequest.predicate = NSPredicate(format: "parentCategory.title MATCHES %@", safeCategory.title!)
+            fetchRequest.sortDescriptors = [sortDescriptor]
+            do
+            {
+                items = try context.fetch(fetchRequest)
+                tableView.reloadData()
+            }catch
+            {
+                print(error.localizedDescription)
+                print("There was an error in loading the items.")
+            }
+        }
     }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
